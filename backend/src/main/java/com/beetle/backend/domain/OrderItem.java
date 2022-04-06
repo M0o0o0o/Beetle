c426d73993a37df002a07fd0469779dd8a7a56c5
Lee-moo <dlandif22@gmail.com>
2022-04-06 15:38:12 +0900

    주문아이템 컬럼 추가, 연관관계 매핑

--- backend/src/main/java/com/beetle/backend/domain/OrderItem.java
+++ backend/src/main/java/com/beetle/backend/domain/OrderItem.java
@@ -3,10 +3,7 @@ package com.beetle.backend.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,4 +13,16 @@ public class OrderItem {
     @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
+
+    // order - orderItem의 관계는 일대다 즉 연관관계의 주인은 orderItemd;lek.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private Long orderPrice;
+    private Long count;
 }
