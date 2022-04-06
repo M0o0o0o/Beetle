56cb842b8b372693087cb3374854a5dd56bf3882
Lee-moo <dlandif22@gmail.com>
2022-04-06 15:49:35 +0900

    값 타입과 도메인을 한군데 모으기 위해서 address package 생성, 값 타입의 naming을 위해서 기존의 Address 이름을 MemberAddress로 변경

---
+++ backend/src/main/java/com/beetle/backend/domain/address/MemberAddress.java
@@ -0,0 +1,23 @@
+package com.beetle.backend.domain.address;
+
+import com.beetle.backend.domain.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class MemberAddress {
+    @Id
+    @GeneratedValue
+    @Column(name = "address_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
+
