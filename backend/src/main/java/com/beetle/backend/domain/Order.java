be2ab24cbdac6eb96a2028327f5bafa361ed3146
Lee-moo <dlandif22@gmail.com>
2022-04-06 15:37:45 +0900

    주문 도메인 컬럼 추가, 연과관계 매핑

---
+++ backend/src/main/java/com/beetle/backend/domain/Order.java
@@ -0,0 +1,32 @@
+package com.beetle.backend.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private LocalDateTime orderDate;
+}
+
